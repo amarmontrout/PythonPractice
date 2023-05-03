caa065bfaaf8d984ca4b6cc7091dccde7b1edc99
amarmontrout <132516474+amarmontrout@users.noreply.github.com>
2023-05-03 17:18:34 -0500

    Update Color Mixer.py
    Simplified code by combining lines. Converted input to lowercase to account for capital letters. Added output to display if the same color is entered twice.

--- Color Mixer.py
+++ Color Mixer.py
@@ -1,18 +1,14 @@
 
-primary_1 = input('Please enter a primary color.\n')
-primary_2 = input('Please enter a primary color.\n')
+p1 = input('Please enter a primary color (Red, Blue or Yellow): ')
+p2 = input('Please enter a primary color (Red, Blue or Yellow): ')
 
-if primary_1 == 'red' and primary_2 == 'blue':
+if (p1.lower() == 'red' and p2.lower() == 'blue') or (p1.lower() == 'blue' and p2.lower() == 'red'):
     print('Your secondary color is purple!')
-elif primary_1 == 'red' and primary_2 == 'yellow':
+elif (p1.lower() == 'red' and p2.lower() == 'yellow') or (p1.lower() == 'yellow' and p2.lower() == 'red'):
     print('Your secondary color is orange!')
-elif primary_1 == 'blue' and primary_2 == 'yellow':
+elif (p1.lower() == 'blue' and p2.lower() == 'yellow') or (p1.lower() == 'yellow' and p2.lower() == 'blue'):
     print('Your secondary color is green!')
-elif primary_1 == 'blue' and primary_2 == 'red':
-    print('Your secondary color is purple!')
-elif primary_1 == 'yellow' and primary_2 == 'red':
-    print('Your secondary color is orange!')
-elif primary_1 == 'yellow' and primary_2 == 'blue':
-    print('Your secondary color is green!')
+elif p1 == p2:
+    print(p1.lower())
 else:
-    print('Incorrect primary color(s). Please try again.')
+    print('Incorrect color(s). Please try again.')
