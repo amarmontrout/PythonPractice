fd3b1598da1ec4a80bd493d0074dde95ca6b1286
amarmontrout <132516474+amarmontrout@users.noreply.github.com>
2023-05-03 17:43:00 -0500

    Update Color Mixer.py
    Used lists to make input validation better and nested code in a while loop.

--- Color Mixer.py
+++ Color Mixer.py
@@ -1,14 +1,20 @@
+primary_colors = ['red', 'blue', 'yellow']
+done = ['done']
 
-p1 = input('Please enter a primary color (Red, Blue or Yellow): ')
-p2 = input('Please enter a primary color (Red, Blue or Yellow): ')
-
-if (p1.lower() == 'red' and p2.lower() == 'blue') or (p1.lower() == 'blue' and p2.lower() == 'red'):
-    print('Your secondary color is purple!')
-elif (p1.lower() == 'red' and p2.lower() == 'yellow') or (p1.lower() == 'yellow' and p2.lower() == 'red'):
-    print('Your secondary color is orange!')
-elif (p1.lower() == 'blue' and p2.lower() == 'yellow') or (p1.lower() == 'yellow' and p2.lower() == 'blue'):
-    print('Your secondary color is green!')
-elif p1 == p2:
-    print(p1.lower())
-else:
-    print('Incorrect color(s). Please try again.')
+while True:
+    p1 = input('Please enter a primary color (Red, Blue or Yellow) or type "done" to exit: ')
+    p2 = input('Please enter a primary color (Red, Blue or Yellow) or type "done" to exit: ')
+    if (p1.lower() and p2.lower() in primary_colors) or (p1.lower() and p2.lower() in done):
+        if (p1.lower() == 'red' and p2.lower() == 'blue') or (p1.lower() == 'blue' and p2.lower() == 'red'):
+            print('Your secondary color is purple!')
+        elif (p1.lower() == 'red' and p2.lower() == 'yellow') or (p1.lower() == 'yellow' and p2.lower() == 'red'):
+            print('Your secondary color is orange!')
+        elif (p1.lower() == 'blue' and p2.lower() == 'yellow') or (p1.lower() == 'yellow' and p2.lower() == 'blue'):
+            print('Your secondary color is green!')
+        elif (p1.lower() in done) or (p2.lower() in done):
+            break
+        else:
+            print(p1.lower())
+    else:
+        print('Incorrect color(s). Try again or type "done" to exit.')
+input('Goodbye.')
